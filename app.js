8dc365b8c8b51621b13c71db98bdee72937a1925
Justin Smith <jasmith4183@gmail.com>
2019-12-16 04:36:13 -0600

    Moved Griddy API grab to power.js

--- app.js
+++ app.js
@@ -22,46 +22,7 @@ var intervalid = setInterval(test, 60000);
 
 
 // http post test code to attempt to connect to griddy api and get json object back
-const data = '{"settlement_point":"LZ_WEST"}';
 
-const options = {
-   hostname: 'app.gogriddy.com',
-   port: 443,
-   path: '/api/v1/insights/getnow',
-   method: 'POST',
-   headers: {
-       'Content-Type': 'application/json',
-       'Content-Length': data.length,
-   },
-   
-}
-
-var secondsUntilRefresh = 0;
-var body ='';
-var parsed = {};
-const req = https.request(options, res => {
-    console.log(`statusCode: ${res.statusCode}`)
-    res.on('data', d => {
-     body += d;
-     console.log(body);
-     try {
-        parsed = JSON.parse(body)
-        secondsUntilRefresh = parseInt(parsed.seconds_until_refresh);
-        console.log(secondsUntilRefresh);
-        console.log(parsed.now.price_ckwh);
-      } catch(err) {
-        console.error(err)
-      }
-      
-    })
-  })
-  
-  req.on('error', error => {
-    console.error(error)
-  })
-  
-  req.write(data)
-  req.end()
  
 
 //Get Griddy price every 5 minutes
--- power.js
+++ power.js
@@ -1,8 +1,44 @@
 // power.js is responsible for connection to griddy and performing all power related calculations and loogging
+const https = require('https')
+// Variables and Constant Declarations
+var secondsUntilRefresh = 0; // Used to store the seconds until new Griddy data is available
+var body =''; // Used to store the body of the response from griddy
+var parsed = {}; // Used to store the parsed JSON object after parsing the body from the http response
 
-
-
-
+// Start of HTTP POST Request Code
+const data = '{"settlement_point":"LZ_WEST"}';
+const options = {
+   hostname: 'app.gogriddy.com',
+   port: 443,
+   path: '/api/v1/insights/getnow',
+   method: 'POST',
+   headers: {
+       'Content-Type': 'application/json',
+       'Content-Length': data.length,
+   },
+}
+const req = https.request(options, res => {
+    console.log(`statusCode: ${res.statusCode}`)
+    res.on('data', d => {
+     body += d;
+    // console.log(body);
+    try {
+        parsed = JSON.parse(body);
+        secondsUntilRefresh = parseInt(parsed.seconds_until_refresh);
+        console.log(secondsUntilRefresh);
+        console.log(parsed.now.price_ckwh);
+     } catch(err) {
+       console.error(err)
+     }
+      
+    })
+  })
+  req.on('error', error => {
+    console.error(error)
+  })
+  req.write(data)
+  req.end()
+// End of HTTP POST Request COde
 
 
 
